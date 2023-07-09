691571ce3aa3cbc34f4c469640ab158d6d013030
niikita17 <121359444+niikita17@users.noreply.github.com>
2023-07-09 10:20:34 +0530

    Update settings.py

--- userdata/settings.py
+++ userdata/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_%wlufzm0*@cm^e%8i4&n!_p+xmedgzi*px4qhs9yj#-68e)af
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
